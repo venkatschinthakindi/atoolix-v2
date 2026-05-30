6bcb114415c63d46aab49741da151320672cccac
venkatschinthakindi <venkatschintthakindi@gmail.com>
2026-05-30 21:25:27 +0530

    footer added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,12 +19,14 @@
 import { AppShell } from "@/components/layout/app-shell";
 import { HeroCommandCenter } from "@/components/dashboard/hero-command-center";
 import { FeaturedTools } from "@/components/dashboard/featured-tools";
+import { Footer } from "@/components/footer/footer";
 
 export default function HomePage() {
   return (
     <AppShell>
       <HeroCommandCenter />
       <FeaturedTools />
+      <Footer />
     </AppShell>
   );
 }
