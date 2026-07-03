3d660107ea92a9fcaa0ff1b350cdc75c3d224e0b
venkatschinthakindi <venkatschintthakindi@gmail.com>
2026-07-03 16:35:21 +0530

    Applied some space around page of tools

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -2,6 +2,7 @@ import ToolsHubClient  from "@/app/tools/toolsHubClient";
 import { Footer } from "@/app/footer/footer";
 import ToolLoader from "@/components/tools/toolLoader";
 import { Suspense } from "react";
+import BackButton from "@/components/ui/backButton";
 
 export const metadata = {
   title: "All Free Online Tools - PDF, Image, Calculator, EMI and Finance Tools",
@@ -12,7 +13,7 @@ export const metadata = {
 export default async function ToolsPage({ params }: any) {
   return (
     <>
-    <div className="app-shell">
+    <div className="app-shell px-6">
       <div className="app-container page-section">
 
       <div className="section-header text-center mt-8">
@@ -21,7 +22,9 @@ export default async function ToolsPage({ params }: any) {
             Browse PDF tools, image converters, compressors, and calculators. Explore all utilities available on this site.
           </p>
         </div>
-        
+        <div className="section-header">
+                      <BackButton />
+                    </div>
       <Suspense fallback={<ToolLoader />}>
         <ToolsHubClient />
       </Suspense>
