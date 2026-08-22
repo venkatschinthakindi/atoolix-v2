9f1a84fcb6270756102cc14e713540c3301f735f
venkatschinthakindi <41695277+venkatschinthakindi@users.noreply.github.com>
2026-08-22 15:44:40 +0530

    fix(config): derive server config from shared config

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,19 +1,5 @@
 import "server-only";
 
-export const serverConfig = {
-  siteName: process.env.NEXT_PUBLIC_SITE_NAME!,
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL!,
-  siteLogoUrl: process.env.NEXT_PUBLIC_SITE_LOGO_URL!,
-  siteDescription: process.env.NEXT_PUBLIC_SITE_DESCRIPTION!,
-  companyName: process.env.NEXT_PUBLIC_SITE_COMPANY_NAME!,
-  supportEmail: process.env.NEXT_PUBLIC_SITE_SUPPORT_EMAIL!,
+import { sharedConfig } from "./shared";
 
-  theme: {
-    default: "dark",
-    accent: "indigo",
-  },
-
-  analytics: {
-    googleAnalyticsId: process.env.NEXT_PUBLIC_GA_ID,
-  },
-} as const;
+export const serverConfig = sharedConfig;
