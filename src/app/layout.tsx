2002d2e5e4a4dbd9176dcdc813cf6972c44146d3
venkatschinthakindi <venkatschintthakindi@gmail.com>
2026-08-24 19:17:39 +0530

    removed commented code line

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ import "@/utility/pascalCase";
 import { serverConfig } from "@/config/server";
 import { PwaProvider } from "@/components/pwa/PwaProvider";
 import InstallButton from "@/components/pwa/InstallButton";
-// import PwaUpdateToast from "@/components/pwa/PwaUpdateToast";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
